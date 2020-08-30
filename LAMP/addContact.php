567a74598d0dff20cd110ad674e70a228f5849e0
adamloree <58012816+adamloree@users.noreply.github.com>
2020-08-30 12:28:25 -0400

    Commons + Verify request type
    Changed the functions to work with Commons.php and added a verification of request type.

--- LAMP/addContact.php
+++ LAMP/addContact.php
@@ -1,9 +1,7 @@
 <?php
-  // Status Codes used by this endpoint
-  const STATUS_SUCCESS = 200;
-  const STATUS_BAD_REQUEST = 400;
-  const STATUS_INTERNAL_ERROR = 500;
-  ////////////////////////////////////////
+  include 'Commons.php';
+
+  verify_request_type('POST');
 
   $user_id = 0;
   $first_name = "";
@@ -11,7 +9,7 @@
   $email = "";
   $phone_number = "";
 
-  $indata = getRequestInfo();
+  $indata = decode_JSON_request();
 
   $user_id = $indata["user_id"];
   $first_name = $indata["first_name"];
@@ -45,37 +43,12 @@
   // Function Declarations //
   ///////////////////////////
 
-  // Check to make sure data is legit
+  // Checks to make sure data is authentic
   function check_data($user_id, $first_name, $last_name, $email, $phone_number) {
     if ($user_id == 0 || $first_name == "" || $last_name == "" || $email == "" || $phone_number == "") {
       return true;
     } else {
       return false;
     }
-  }
-
-  // Attempts to connect to the database. On failure, it sends an error response and quits
-  function connect_to_db() {
-      $conn = new mysqli("10.0.0.4", "contacts_app", "#oigCH10*oq^", "contacts_app");
-      if($conn->connect_error) {
-          send_json_response($STATUS_INTERNAL_ERROR, (object)array(
-              'data' => NULL,
-              'error' => 'Could not connect to database',
-          ));
-          exit();
-      }
-      return $conn;
-  }
-
-  // decodes json
-  function getRequestInfo() {
-		return json_decode(file_get_contents('php://input'), true);
-	}
-
-  // Utility for sending back a json response
-  function send_json_response($status, $data) {
-      header('Content-Type: application/json');
-      http_response_code($status);
-      echo(json_encode($data));
   }
 ?>
